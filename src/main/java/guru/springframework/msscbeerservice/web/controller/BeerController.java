54ab310cd8d934ef5da687bacbf4c5b6e0f33bed
Heliel Amaro <hfamaro@hotmail.com>
2023-09-19 23:16:52 -0400

    Adding controller tests

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ import guru.springframework.msscbeerservice.web.model.BeerDto;
 @RestController
 public class BeerController {
     
-    @GetMapping("/{beerid}")
+    @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerByID(@PathVariable("beerId") UUID beerId){
         // TODO impl
         return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
@@ -29,8 +30,8 @@ public class BeerController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @PostMapping("/{beerid}")
-    public ResponseEntity<?> getBeerByID(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+    @PutMapping("/{beerId}")
+    public ResponseEntity<?> updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
         // TODO impl
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
